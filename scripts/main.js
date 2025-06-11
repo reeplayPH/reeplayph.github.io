386349dc6a2ca8d0c83f9d93702e603b8299158c
reeplayPH <reel.and.play@gmail.com>
2025-06-11 19:02:13 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -247,7 +247,7 @@ function populateRankingEntry(housemate, currRank) {
 	<div class="ranking__entry-icon-border ${housemate.duoname2color.toLowerCase()}-rank-border" data-rankid="${currRank-1}"></div>
 	</div>
 	<div class="ranking__entry-icon-badge bg-${housemate.duoname2color.toLowerCase()}">${RankTag}</div>
-	${nominated ? '<div class="ranking__entry-nominated"></div>' : ''}
+	${nominated ? '<div class="ranking__entry-nominated ${bigWinnerClass}"></div>' : ''}
 	</div>
 	<div class="ranking__row-text">
 	<div class="name"><strong>${housemate.duoname.toUpperCase()}</strong></div>
